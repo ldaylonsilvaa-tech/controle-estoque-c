d5be79cce5373bd3af3c0756c50fc02642368186
ldaylonsilvaa-tech <ldaylon.silvaa@gmail.com>
2026-07-22 14:46:29 +0000

    Adiciona menu principal com loop e if-else

--- estoque.c
+++ estoque.c
@@ -12,6 +12,30 @@ typedef struct {
 } Produto;
 
 int main() {
-    printf("Sistema de Controle de Estoque\n");
+    int opcao;
+
+    do {
+        printf("\n===== SISTEMA DE CONTROLE DE ESTOQUE =====\n");
+        printf("1 - Cadastrar produto\n");
+        printf("2 - Listar produtos\n");
+        printf("3 - Buscar produto\n");
+        printf("0 - Sair\n");
+        printf("Escolha uma opcao: ");
+        scanf("%d", &opcao);
+
+        if (opcao == 1) {
+            printf("Voce escolheu Cadastrar produto.\n");
+        } else if (opcao == 2) {
+            printf("Voce escolheu Listar produtos.\n");
+        } else if (opcao == 3) {
+            printf("Voce escolheu Buscar produto.\n");
+        } else if (opcao == 0) {
+            printf("Encerrando o sistema...\n");
+        } else {
+            printf("Opcao invalida! Tente novamente.\n");
+        }
+
+    } while (opcao != 0);
+
     return 0;
 }
